54c7ffaea5c0ad15147cbaa163d3d366192c425a
samuelrapp <slrapp@ucsc.edu>
2021-02-11 12:11:47 -0800

    correcting error: deleting rows in forloop  caused index error

--- JuypterNotebook/annotating_tree/annotating_tree.R
+++ JuypterNotebook/annotating_tree/annotating_tree.R
@@ -6,40 +6,58 @@ OTL_dataframe  <- read.csv(file = "NPS_AIS_GENUS_taxon_names_according_to_OTL.tx
 CoverageMatrix_NCBI_genus_DF<- read.csv(file = "CovM_NCBI_genus_NPS.csv")
 Output_DF <- CoverageMatrix_NCBI_genus_DF
 
-#forloop going through Output_DF
-#get
-#
-  #inner forloop going through OTL_dataframe 
-  #if find grep match replace Output_DF[i,1] with OTL_dataframe[j,1]
-   #found anything = true
-
-#if found anything == false
-#delete row
-
-#found anything = false turned
-
+rows_to_remove <- c()
 for (i in 1:nrow(Output_DF)) {
   needle <- Output_DF[i,1]
-  #print(needle)
   foundHaystack <- "FALSE"
   for(j in 1:nrow(OTL_dataframe))
   {
     haystack<-OTL_dataframe[j,1]
-   # print(haystack)
     if(grepl(needle, haystack, fixed = TRUE) == T)
        {
         haystack <- trimws(haystack, "r")
-         Output_DF[i,1] <- haystack
-       #  print("found")
-         foundHaystack <-"TRUE"
+        Output_DF[i,1] <- haystack
+        foundHaystack <-"TRUE"
     }
   }
   if(foundHaystack == "FALSE") #no OTL value found
   { 
-    print(Output_DF[i,1])
-    Output_DF <- Output_DF[-c(i),]#delete row
+    rows_to_remove <- c(rows_to_remove, i)
   }
 }
+
+Output_DF <- Output_DF[-rows_to_remove,]#delete rows where CovMatrixvalue wasn't replaced,
+#note there are also Out
+
+needle <-"Cordylophora"
+holder <- "x"
+for(j in 1:nrow(OTL_dataframe))
+{
+  haystack<-OTL_dataframe[j,1]
+  # print(haystack)
+  if(grepl(needle, haystack, fixed = TRUE) == T)
+  {
+    #haystack <- trimws(haystack, "r")
+    print(haystack)
+    #Output_DF[i,1] <- haystack
+    #  print("found")
+    #
+    holder <- haystack
+    foundHaystack <-"TRUE"
+  }
+}
+
+#psuedo codee
+#forloop going through Output_DF
+#get
+#
+#inner forloop going through OTL_dataframe 
+#if find grep match replace Output_DF[i,1] with OTL_dataframe[j,1]
+#found anything = true
+
+#if found anything == false
+#delete row
+#found anything = false turned
 
 write.csv(Output_DF, file = "OTT_IDD_Annotated", row.names = FALSE)
 #to download full dataset
